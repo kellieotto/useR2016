fbe1750f411f7ae92909cc32e1a7e0f5e32ceded
Kellie Ottoboni <kellieotto@berkeley.edu>
2016-06-25 09:45:25 -0700

    Change some lines in nsgk_example to be more compact

--- src/nsgk_example.R
+++ src/nsgk_example.R
@@ -16,12 +16,12 @@ time_stamps <- c(36, 32, 35, 37, 31, 35, 40, 32)
 tag1 <- nsgk_mat[[1]]
 
 # Find the distribution of the IRR test statistic for each video
-video_specific_results <- lapply(tag1, function(x){
-  res <- irr_ts_distribution(x, num_perm = 1000,    
-                            keep_dist = TRUE, seed = 101)
+results_by_video <- lapply(tag1, function(x){
+  res <- irr_ts_distribution(x, reps = 1000,    
+                             keep_dist = TRUE, seed = 101)
 })
-tag_distribution <- sapply(video_specific_results, function(x) x$dist)
-tag_pvalues      <- sapply(video_specific_results, function(x) x$pvalue)
+tag_distr    <- sapply(results_by_video, function(x) x$dist)
+tag_pvalues  <- sapply(results_by_video, function(x) x$pvalue)
 
 # Combine across videos with NPC
 tag_npc_res <- irr_npc_distribution(tag_distribution, size = time_stamps, pvalues = tag_pvalues)
